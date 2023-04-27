a6dc19e75de4f6668c6d725605ab50b737eb8016
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-04-27 13:05:16 -0500

    RT-30 second component

---
+++ counter-app/src/HelloWorld.jsx
@@ -0,0 +1,5 @@
+export const HelloWorld = () => {
+    return (
+        <h2>Hello World this is my first message</h2>
+    );
+};
--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
 import { App } from './FirstApp';
+import { HelloWorld } from './HelloWorld';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
+        <HelloWorld />
     </React.StrictMode>
 );
 
