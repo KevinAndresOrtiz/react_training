e05347706a35917e28f6893ecc90fa519ca28afb
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-04-27 12:00:06 -0500

    changed text in react.js

--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM  from 'react-dom/client';
 
 function App() {
-    return (<h1>Hola Mundo</h1>);
+    return (<h1>First hello world in react</h1>);
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
