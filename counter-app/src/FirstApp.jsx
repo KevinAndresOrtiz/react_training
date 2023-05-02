54c9591a54a425ecf3934fc3b18b31aa404340ec
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-02 10:24:08 -0500

    RT-32 variables and function

--- counter-app/src/FirstApp.jsx
+++ counter-app/src/FirstApp.jsx
@@ -1,9 +1,14 @@
 import { Fragment } from "react";
 
+const getSaludo = () => {
+    return 'Este es mi primer llamado de una funcion'
+}
+
 export const App = () => {
     return (
         <Fragment>
-            <h1>Kevin</h1>
+            <h1>Hola Mundo</h1>
+            {getSaludo()}
             <p>Tengo 29 años</p>
         </Fragment>
     );
