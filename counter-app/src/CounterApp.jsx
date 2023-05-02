47d3324f83920a6b2ea9df7f255fac1a6187faa1
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-02 11:41:47 -0500

    RT-38 added onclick event

--- counter-app/src/CounterApp.jsx
+++ counter-app/src/CounterApp.jsx
@@ -2,10 +2,18 @@ import PropTypes from "prop-types";
 
 
 export const CounterApp = ({ counter }) => {
+    function handleAdd() {
+        console.log('+1');
+        counter = 1000;
+    }
+
     return(
         <>
         <h2>Counter App</h2>
         <p>{ counter }</p>
+        <button onClick={ handleAdd }>
+            +1
+        </button>
         </>
     )
 };
