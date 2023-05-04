fb424f7586b436d98e158d13b2d3663cff340c64
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-04 13:45:30 -0500

    RT-46 customizing elements with props

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,7 +6,15 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 function App(){
-    return (<h1>{ new Date().toLocaleTimeString() }</h1>);
+    const inputType = 'number';
+    const message = 'Enter age';
+    const minValue = 5;
+    return (
+            <>
+            <input type={inputType} min={minValue} max={100} />
+            <input type={inputType} min={minValue} max={100} list={[1, 2, 3]} style={{color: 'red'}} alt={message} />
+            </>
+            );
 }
 
 root.render(<App />);
