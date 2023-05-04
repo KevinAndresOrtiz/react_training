b26709843ccfcbdf1f8e1075c728726d1379de70
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-04 14:12:14 -0500

    RT-49 Extracting Components

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,21 +1,9 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
+import App from "./App";
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
-
-function App(){
-    // const inputType = 'number';
-    // const message = 'Enter age';
-    // const minValue = 5;
-    return (
-            <>
-            {/* <input type={inputType} min={minValue} max={100} />
-            <input type={inputType} min={minValue} max={100} list={[1, 2, 3]} style={{color: 'red'}} alt={message} /> */}
-            <div style={{ textDecoration:'none', padding: '15px'}}>This is inlyne style , as you can see always you need to pass an object </div>
-            </>
-            );
-}
 
 root.render(<App />);
