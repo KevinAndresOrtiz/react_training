40a635addac796ed1102909a884f6221104b3ecb
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-04 14:02:37 -0500

    RT-48 applying inline style

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,13 +6,14 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 function App(){
-    const inputType = 'number';
-    const message = 'Enter age';
-    const minValue = 5;
+    // const inputType = 'number';
+    // const message = 'Enter age';
+    // const minValue = 5;
     return (
             <>
-            <input type={inputType} min={minValue} max={100} />
-            <input type={inputType} min={minValue} max={100} list={[1, 2, 3]} style={{color: 'red'}} alt={message} />
+            {/* <input type={inputType} min={minValue} max={100} />
+            <input type={inputType} min={minValue} max={100} list={[1, 2, 3]} style={{color: 'red'}} alt={message} /> */}
+            <div style={{ textDecoration:'none', padding: '15px'}}>This is inlyne style , as you can see always you need to pass an object </div>
             </>
             );
 }
