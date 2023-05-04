6753fa16fa51277055a91e8573a3e81d25098252
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-04 11:49:56 -0500

    RT-43 printing variables javascript

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,7 +6,7 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 function App(){
-    return (<h1>Hi there</h1>);
+    return (<h1>{ new Date().toLocaleTimeString() }</h1>);
 }
 
 root.render(<App />);
