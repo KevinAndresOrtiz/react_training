db17c1d8ce9b7d80fef9913dcb42000a703aa71c
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-08 16:29:46 -0500

    RT-60 promises test

---
+++ Test-React-Component/test/09-promesas.test.js
@@ -0,0 +1,12 @@
+import { getHeroeByIdAsync } from "../src/base-pruebas/09-promesas";
+
+describe('Prueba en 09-promesas', () => {
+    test('getHeroesByIdAsync debe de retornar un heroe', (done)=>{
+        const id = 1;
+        getHeroeByIdAsync(id).then(hero => {
+            expect(hero).toStrictEqual({id: 1, name: "Batman", owner: "DC"});
+            done();
+        })
+        
+    });
+});
