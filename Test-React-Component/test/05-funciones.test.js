76e70a741b42daa4fcbce0e943e0b231614f30dd
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-08 12:06:48 -0500

    RT-57 added test funciones

---
+++ Test-React-Component/test/05-funciones.test.js
@@ -0,0 +1,24 @@
+import { getUser, getUsuarioActivo } from "../src/base-pruebas/05-funciones";
+
+describe('Pruebas en 5-funciones', () => {
+    test('getUser debe de retornar un objeto', () => {
+        const testUser = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        };
+
+        const user = getUser();
+
+        expect( testUser ).toStrictEqual( user );
+    });
+
+    test('getUsuarioActivo debe de retornar un objeto', () => {
+        const name = 'Kevin';
+        const testUsuarioActivo = {
+            uid: 'ABC567',
+            username: name
+        }
+
+        expect(testUsuarioActivo).toStrictEqual(getUsuarioActivo(name))
+    });
+});
