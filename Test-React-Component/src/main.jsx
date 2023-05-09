61406d3eb33aaf1aa5a73aacc12b96548a5137c7
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-09 11:54:30 -0500

    RT-64 setup testing components

--- Test-React-Component/src/main.jsx
+++ Test-React-Component/src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
-import './index.css'
+// import './index.css'
 import { FirstApp } from './FirstApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp title='Mi name is Kevin' 
-              subTitle='Estoy aprendiendo React' 
-              name='Kevin Andres Ortiz Merchan' />
+    <FirstApp title='Mi name is Kevin' />
   </React.StrictMode>,
 )
