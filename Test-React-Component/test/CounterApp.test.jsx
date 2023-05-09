801208318c597d98506d44738825a267c7f6a42f
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-09 17:11:03 -0500

    RT-69 added fireEvent and screen in testing component

--- Test-React-Component/test/CounterApp.test.jsx
+++ Test-React-Component/test/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
 describe('Pruebas en <CounterApp />', ()=> {
@@ -14,5 +14,22 @@ describe('Pruebas en <CounterApp />', ()=> {
         render(<CounterApp counter={counterValue} />);
         expect(screen.getByText(counterValue)).toBeTruthy();
         expect(screen.getByText(counterValue).innerHTML).toBe(counterValue.toString());
+    });
+
+    test('debe de incrementar con el boton +1', () => {
+        render(<CounterApp counter={10} />);
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText('11')).toBeTruthy();
+    });
+    test('debe de decrementar con el boton -1', () => {
+        render(<CounterApp counter={10} />);
+        fireEvent.click(screen.getByText('-1'));
+        expect(screen.getByText('9')).toBeTruthy();
+    });
+
+    test('debe de funcionar el boton de reset', () => {
+        render(<CounterApp counter={10} />);
+        fireEvent.click(screen.getByText('Reset'));
+        expect(screen.getByText('10')).toBeTruthy();
     });
 });
