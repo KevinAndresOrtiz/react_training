bdc92f63c606c6a6fe1e66b6cce790df98208ae9
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-09 13:00:36 -0500

    RT-66 added getByTestId and getByallTEXT

--- Test-React-Component/test/FirstApp.test.jsx
+++ Test-React-Component/test/FirstApp.test.jsx
@@ -8,12 +8,25 @@ describe("Pruebas en <FirstApp />", () => {
     expect(container).toMatchSnapshot();
   });
 
-  test('debe de mostrar el titulo en un h1', ()=>{
+  test("debe de mostrar el titulo en un h1", () => {
     const title = "Hola mi nombre es Kevin";
-    const { container, getByText } = render(<FirstApp title={title} />);
+    const {  getByText, getByTestId } = render(
+      <FirstApp title={title} />
+    );
 
     expect(getByText(title)).toBeTruthy();
-    const h1 = container.querySelector('h1');
-    expect(h1.innerHTML).toContain(title);
+    expect(getByTestId("test-title")).toBeTruthy();
+    expect(getByTestId("test-title").innerHTML).toContain(title);
+    // const h1 = container.querySelector('h1');
+    // expect(h1.innerHTML).toContain(title);
+  });
+
+  test("debe de mostrar el subtitulo enviado por props", () => {
+    const title = "Hola soy Goku";
+    const subtitle = "soy un subtitulo";
+    const { getAllByText } = render(
+      <FirstApp title={title} subTitle={subtitle} />
+    );
+    expect(getAllByText(subtitle).length).toBe(2);
   });
 });
