1f02f7bd61f7b8704adb10a94e09ee3ba7d7035f
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-10 13:20:23 -0500

    RT-122 component list

---
+++ todo-machine/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprendamos React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function TodoItem() {
+  return(
+    <li>
+      <span>V</span>
+      <p>Llorar con la llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+export default App;
