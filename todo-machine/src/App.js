6287fadd86d240600d899ae615ef5906f1461dfe
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-11 15:14:44 -0500

    RT-123 Components todo machine

--- todo-machine/src/App.js
+++ todo-machine/src/App.js
@@ -1,38 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
 
 function App() {
   return (
     <div className="App">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos React
-        </a>
-      </header>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList >
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+      {/* <CreateTodoButton /> */}
     </div>
   );
 }
 
-function TodoItem() {
-  return(
-    <li>
-      <span>V</span>
-      <p>Llorar con la llorona</p>
-      <span>X</span>
-    </li>
-  );
-}
 
 export default App;
---
+++ todo-machine/src/TodoItem.js
@@ -0,0 +1,9 @@
+export function TodoItem() {
+    return(
+      <li>
+        <span>V</span>
+        <p>Llorar con la llorona</p>
+        <span>X</span>
+      </li>
+    );
+  }
