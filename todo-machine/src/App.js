ac3265009d0c2f692164ff70880e5bd10c7f74c2
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-15 12:25:34 -0500

    RT-128 states derivated and pass state from parent to child

--- todo-machine/src/App.js
+++ todo-machine/src/App.js
@@ -3,23 +3,30 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import React from 'react';
+import React, { useState } from 'react';
 
 const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso de react.js', completed: false },
+  { text: 'Tomar el curso de react.js', completed: true },
   { text: 'Llorar con la llorona', completed: false },
   { text: 'Curso 2 de react.js', completed: false },
   { text: 'Tomar cursos de cloudguru', completed: false },
 ];
 
 function App() {
+  const [todos, setTodos] = useState(defaultTodos);
+  const [searchValue, setSearchValue] = useState('');
+  const completedTodos = todos.filter((value) => value.completed === true).length;
+  const totalTodos = todos.length;
+
   return (
     <React.Fragment>
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch 
+            searchValue={searchValue} 
+            setSearchValue={setSearchValue}/>
       <TodoList >
-        {defaultTodos.map(todo => (<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
+        {todos.map(todo => (<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
       </TodoList>
       <CreateTodoButton />
     </React.Fragment>
--- todo-machine/src/TodoSearch.js
+++ todo-machine/src/TodoSearch.js
@@ -1,8 +1,5 @@
-
-import { useState } from 'react';
 import './TodoSearch.css'
-export function TodoSearch() {
-    const [searchValue, setSearchValue] = useState();
+export function TodoSearch({searchValue, setSearchValue}) {
     return (
       <input placeholder="Cortar Cebolla" className="TodoSearch" value={searchValue} onChange={(event) => {setSearchValue(event.target.value)}}/>
     );
