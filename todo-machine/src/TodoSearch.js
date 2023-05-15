c5be8b9649ef11fea5098f4bdcfc2c2ef1fceae1
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-15 11:58:47 -0500

    RT-127 useState

--- todo-machine/src/TodoSearch.js
+++ todo-machine/src/TodoSearch.js
@@ -1,9 +1,9 @@
+
+import { useState } from 'react';
 import './TodoSearch.css'
 export function TodoSearch() {
+    const [searchValue, setSearchValue] = useState();
     return (
-      <input placeholder="Cortar Cebolla" className="TodoSearch" onChange={(event) => {
-                                                                        console.log('Escribiste en el todoSearch')
-                                                                        console.log(event.target.value);
-                                                                        }}/>
+      <input placeholder="Cortar Cebolla" className="TodoSearch" value={searchValue} onChange={(event) => {setSearchValue(event.target.value)}}/>
     );
   }
