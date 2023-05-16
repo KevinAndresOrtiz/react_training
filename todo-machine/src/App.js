530fb52104380b2289d76983e3c67a4653b5e708
Kevin Andres Ortiz Merchan - vn559a9 <Kevin.Kevin.Ortiz@walmart.com>
2023-05-16 16:22:47 -0500

    RT-133 localStorage

--- todo-machine/src/App.js
+++ todo-machine/src/App.js
@@ -5,31 +5,48 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import React, { useState } from 'react';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso de react.js', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
-  { text: 'Curso 2 de react.js', completed: false },
-  { text: 'Tomar cursos de cloudguru', completed: false },
-];
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el curso de react.js', completed: false },
+//   { text: 'Llorar con la llorona', completed: false },
+//   { text: 'Curso 2 de react.js', completed: false },
+//   { text: 'Tomar cursos de cloudguru', completed: false },
+// ];
+
+
 
 function App() {
-  const [todos, setTodos] = useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+  if(!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+
+  }
+  parsedTodos = JSON.parse(localStorageTodos);
+  const [todos, setTodos] = useState(parsedTodos);
   const [searchValue, setSearchValue] = useState('');
   const completedTodos = todos.filter((value) => value.completed === true).length;
   const totalTodos = todos.length;
   const searchedTodos = todos.filter((value) => value.text.toLowerCase().includes(searchValue.toLocaleLowerCase()));
 
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('TODOS_V1',JSON.stringify(newTodos));
+    setTodos(newTodos);
+  }; 
+
   const completeTodos = (text) => {
     const newTodos = [...todos];
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos)
+    saveTodos(newTodos)
   };
 
   const deleteTodos = (text) => {
     const todosDeleted = todos.filter((todo) => todo.text !== text);
-    setTodos(todosDeleted);
+    saveTodos(todosDeleted);
   };
 
   return (
